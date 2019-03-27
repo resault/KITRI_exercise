0bbbd9844f74d5393d1139f60745c5077ebf9ac2
resault <resault.m@gmail.com>
2019-03-27 22:25:44 +0900

    3/26

--- exercise/src/study0326/Review.java
+++ exercise/src/study0326/Review.java
@@ -1,5 +1,11 @@
 package study0326;
 
 public class Review {
-	
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb1 = new StringBuffer();
+		System.out.println("크기 : " + sb1.capacity() + " 문자열 수 : " + sb1.length());//16 0
+		
+	}
 }
