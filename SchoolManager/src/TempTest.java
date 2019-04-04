e34ccb8fc1c8acbf1be091a4cdf0f6d42d7812dd
resault <resault.m@gmail.com>
2019-04-04 18:28:08 +0900

    4/4

---
+++ SchoolManager/src/TempTest.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.ArrayList;
+
+import com.kitri.haksa.data.HaksaDto;
+
+public class TempTest {
+
+	public static void main(String[] args) {
+
+		int a = "0".charAt(0);
+		System.out.println(a);
+
+	}
+}
