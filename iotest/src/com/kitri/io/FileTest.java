e6041e7130d7398f9af6cdae3fc6e71f57cc1a4f
resault <resault.m@gmail.com>
2019-04-05 00:43:41 +0900

    4/4

--- iotest/src/com/kitri/io/FileTest.java
+++ iotest/src/com/kitri/io/FileTest.java
@@ -8,27 +8,27 @@ public class FileTest {
 		InputStream in = null;
 		OutputStream out = null;
 		try {
-			File infile = new File("e:\\iotest\\hello.txt");
-			in = new FileInputStream(infile);
-			long length = infile.length();
-			byte b[] = new byte[(int)length];
-			int x = in.read(b);
-			String str = new String(b, 0, x);
-			System.out.println(x + "bytes read!!!");
-			System.out.println(str);
+			File infile = new File("d:\\iotest\\hello.txt");//pathname으로 File 인스턴스 생성
+			long length = infile.length();//파일의 크기 리턴
+			byte b[] = new byte[(int)length];//byte 배열의 크기를 파일 크기로 지정
+			in = new FileInputStream(infile);//InputStream은 추상클래스이므로, 하위클래스로 생성
+			int x = in.read(b);//파일의 내용을 byte단위로 읽어와서 배열에 저장하고, 읽어온 byte수를 반환
+			String str = new String(b, 0, x);//배열 b의 인덱스 0부터 인덱스 x(파일크기)까지를 String으로 생성
+			System.out.println(x + "bytes read!!!");//파일크기(읽어온 byte 수)
+			System.out.println(str);//파일 내용
 		
-			File outfile = new File("e:\\iotest\\hello_copy.txt");
+			File outfile = new File("e:\\iotest\\hello_copy.txt"); 
 			out = new FileOutputStream(outfile);
-			out.write(b);
+			out.write(b);//infile의 내용을 새로 만든 파일에 출력하여 저장
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} catch (IOException e) {//얘가 위로 올라가면 안됨!
+		} catch (IOException e) {//FileNotFoundE-은 IOE-의 하위클래스이므로 IOE-가 먼저 나오면 파일이 없어도 IOE-로 처리됨
 			e.printStackTrace();
 		} finally {
 			try {
 				if(in != null)
-					in.close();
+					in.close();//파일 닫지 않으면 저장되지 않음
 				if(out != null)
 					out.close();
 			} catch (IOException e) {
--- iotest/src/com/kitri/io/ReaderTest.java
+++ iotest/src/com/kitri/io/ReaderTest.java
@@ -6,13 +6,13 @@ import java.io.*;
 public class ReaderTest {
 	
 	public static void main(String[] args) {
-		Reader in = null;
+		Reader in = null; //Reader : 문자 단위 입력을 위한 최상위 클래스
 		try {
-			in = new InputStreamReader(System.in);
+			in = new InputStreamReader(System.in);// Reader가 추상클래스이므로, 하위클래스를 이용해서 생성
 			System.out.print("입력 : ");
-			char[] c = new char[100];
-			int x = in.read(c);
-			System.out.println("x == " + x);
+			char[] c = new char[100];// 배열은 생성시에 크기를 지정해야 함
+			int x = in.read(c); // char 단위로 읽어들인 개수를 int로 반환함
+			System.out.println("x == " + x); // 엔터는 2byte이므로 입력한 글자수 + 2가 반환됨 (한/영 동일)
 			String str = new String(c, 0, x-2);
 			System.out.println(str);
 			
@@ -23,6 +23,5 @@ public class ReaderTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
