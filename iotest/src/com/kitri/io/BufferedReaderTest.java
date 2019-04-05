2d3e4a6376a1241782bcdfb4e2ca7f8fd0922fef
resault <resault.m@gmail.com>
2019-04-05 18:28:40 +0900

    4/5

--- iotest/src/com/kitri/io/BufferedReaderTest.java
+++ iotest/src/com/kitri/io/BufferedReaderTest.java
@@ -8,24 +8,26 @@ public class BufferedReaderTest {
 		BufferedReader fin = null;
 		FileWriter fw = null; // String으로 출력하는 method가 있기 때문에 BufferedWriter 쓸 필요 없음
 		try {
+			//console에서 파일 이름(경로) 받아오기
 //			InputStream is = System.in;
 //			Reader r = new InputStreamReader(is);
 //			BufferedReader in = new BufferedReader(r);
 			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// r과 is를 사용할 일 없으므로
 			System.out.print("읽을 파일이름 : ");
-			String inFile = in.readLine(); //readLine는 엔터를 기준으로 읽어들임(but, 엔터는 버퍼에 저장되지 않음)
-//			System.out.println("infile == "+ inFile);
+			String inFile = in.readLine(); //BufferedReader의 readLine() : 엔터를 기준으로 읽어서 String으로 반환함(but, 엔터는 버퍼에 저장되지 않음)
+			System.out.println("infile == "+ inFile);
 
 			System.out.print("복사할 파일이름 : ");
 			String outFile = in.readLine();
 			fw = new FileWriter(new File(outFile));
 			
-//			fin = new BufferedReader(new InputStreamReader(new FileInputStream(inFile)));
+			//파일에서 내용 읽어오기
+//			fin = new BufferedReader(new InputStreamReader(new FileInputStream(inFile)));//FileInputStream의 생성자는 File을 인자값으로 받는데, 이렇게 하면 String을 인자값으로 준게 되어버림
 			fin = new BufferedReader(new InputStreamReader(new FileInputStream(new File(inFile))));
 			String str = null;
-			while((str = fin.readLine()) != null) {
-				System.out.println(str);
-				fw.write(str + "\n\r");
+			while((str = fin.readLine()) != null) {//buffer에 저장된 파일내용을 한줄단위로 읽어서 더 읽을 내용이 없으면 while문이 끝남
+				System.out.println(str);//한줄 단위로 읽어온 파일 내용을 출력
+				fw.write(str + "\n\r");//복사할 파일에 File의 write()를 이용하여 내용 출력 //파일 닫아야 반영됨
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
--- iotest/src/com/kitri/io/FileReaderTest.java
+++ iotest/src/com/kitri/io/FileReaderTest.java
@@ -8,19 +8,28 @@ public class FileReaderTest {
 		FileReader fr = null;
 		FileWriter fw = null;
 		try {
-			File inFile = new File("E:\\iotest\\hello.txt");
-			fr = new FileReader(inFile);
-			System.out.println(fr);
-			long length = inFile.length();
-			char c[] = new char[(int) length];
-			int x = fr.read(c);
+			//파일 특정
+			File inFile = new File("E:\\iotest\\hello.txt"); //pathname으로 File 인스턴스 생성
+			fr = new FileReader(inFile); //FileReader는 char단위로 데이터를 읽어옴. File인스턴스를 인자값으로 입력해서 생성
+			System.out.println(fr); //FileReader에 입력된(?) 주소값이 출력됨
+			
+			//파일 내용 읽어오기
+			long length = inFile.length(); //File의 length() : 파일 크기를 long으로 반환
+			char c[] = new char[(int) length]; //파일 크기만큼의 char배열 생성 //cf.byte 단위로 읽어오는 InputStream을 쓸 때는 byte 배열 생성
+			int x = fr.read(c); //FileReader의 상위클래스인 InputStreamReader의 read() : char단위로 읽어온 내용을 배열로 저장하고, char개수 int로 반환
 			System.out.println(x + "characters read!!!");
-			String str = new String(c);
-			System.out.println(c);
+			
+			//파일 내용 출력
+//			for(int i=0;i<(int)length;i++) { //char배열을 for문으로 돌려서 내용을 출력할 수도 있음
+//				System.out.print(c[i]);
+//			}
+			String str = new String(c); // but, 이렇게 char 배열로 String 인스턴스를 생성하여 출력하는 것이 보다 효율적임!
+			System.out.println(c);    
 
+			
 			File outFile = new File("E:\\iotest\\hello_copy2.txt");
 			fw = new FileWriter(outFile);
-			fw.write(str); // 파일을 닫지 않으면 입력되지 않음
+			fw.write(str); // FileWriter의 write() char단위로 데이터 출력. 파일을 닫지 않으면 메모리상에서만 출력하기에 파일열어도 내용 없음
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
