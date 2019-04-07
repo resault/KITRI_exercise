50727983463a17244e002a6a9e028c30cb25f321
resault <resault.m@gmail.com>
2019-04-07 23:26:58 +0900

    4/7

--- IoTest/src/com/kitri/io/Notepad.java
+++ IoTest/src/com/kitri/io/Notepad.java
@@ -37,6 +37,7 @@ public class Notepad extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
 	}
 
 }
