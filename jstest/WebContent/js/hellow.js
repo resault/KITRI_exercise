a5439ba9c72ac0fd581510d04e75b4b2c67e32cc
resault <resault.m@gmail.com>
2019-04-22 12:34:48 +0900

    4/22

---
+++ jstest/WebContent/js/hellow.js
@@ -0,0 +1,5 @@
+function hello(name) {//data형식 안씀
+	//alert, console : java에서 sysout해서 디버깅한 것처럼 쓸 수 있음
+	alert("Hello2 " + name + "!!!"); // 알림창
+	console.log("안녕2" + name + "!!!"); // 개발자도구의 콘솔창
+}
