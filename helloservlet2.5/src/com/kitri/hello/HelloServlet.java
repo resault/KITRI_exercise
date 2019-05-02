4d574119829ac4f372d6ad8621e46adab1be1197
resault <resault.m@gmail.com>
2019-05-02 19:55:25 +0900

    5/2

--- helloservlet2.5/src/com/kitri/hello/HelloServlet.java
+++ helloservlet2.5/src/com/kitri/hello/HelloServlet.java
@@ -4,21 +4,25 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.print("<html>");
 		out.print("	<body>");
-		out.print("	hello servlet!!!<br>");
-		out.print("	안녕 서블릿!!!");
+		out.print("	Hello Servlet!!!");
 		out.print("	</body>");
 		out.print("</html>");
+		
 	}
-	
+
 	
 }
