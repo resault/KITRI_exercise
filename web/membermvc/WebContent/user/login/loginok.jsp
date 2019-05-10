aba4ed0f4164d2e386a1144a7f0339e92b2de5cb
resault <resault.m@gmail.com>
2019-05-10 18:33:56 +0900

    5/10

--- web/membermvc/WebContent/user/login/loginok.jsp
+++ web/membermvc/WebContent/user/login/loginok.jsp
@@ -1,8 +1,39 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8" import="com.kitri.member.model.MemberDto,com.kitri.util.MoveUrl"%>
 <%@ include file="/template/header.jsp" %>
 <%
-String name = request.getParameter("name");
+MemberDto memberDto = (MemberDto) session.getAttribute("userInfo");
+if(memberDto != null) {
 %>
-<strong><%=name%></strong>님 안녕하세요.<br>
+<script type="text/javascript">
+function deleteMember() {
+	if(confirm("정말뢰 탈퇴??")) {
+		document.location.href = "<%=root%>/user?act=deletemember";
+	}
+}
+</script>
+<strong><%=memberDto.getName()%>(<%=memberDto.getId()%>)</strong>님 안녕하세요.<br>
+<a href="<%=root%>/user?act=logout">로그아웃</a>
+<a href="<%=root%>/user?act=mvmodify">정보수정</a>
+<a href="#" onclick="javascript:deleteMember();">회원탈퇴</a>
+<%
+	if("java2".equals(memberDto.getId())) {
+%>
+<a href="<%=root%>/admin?act=memberlist">관리자</a>
+<%
+	}
+} else {
+	MoveUrl.redirect(request, response, "/user?act=mvlogin");
+}
+%>
 <%@ include file="/template/footer.jsp" %>
+
+
+
+
+
+
+
+
+
+
--- web/membermvc/src/com/kitri/member/controller/MemberController.java
+++ web/membermvc/src/com/kitri/member/controller/MemberController.java
@@ -1,80 +1,105 @@
 package com.kitri.member.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 import com.kitri.member.model.MemberDetailDto;
-import com.kitri.member.model.ZipcodeDto;
+import com.kitri.member.model.MemberDto;
 import com.kitri.member.model.service.MemberServiceImpl;
-import com.kitri.util.SiteConstance;
 
-@WebServlet("/user")
-public class MemberController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class MemberController {
+	
+	private static MemberController memberController;
+	
+	static { 
+		memberController = new MemberController();
+	}
+	
+	private MemberController() {}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public static MemberController getMemberController() {
+		return memberController;
+	}
+	
+	public String register(HttpServletRequest request, HttpServletResponse response) {
+		String path = "/index.jsp";
+		MemberDetailDto memberDetailDto = new MemberDetailDto();
+		memberDetailDto.setName(request.getParameter("name"));
+		memberDetailDto.setId(request.getParameter("id"));
+		memberDetailDto.setPass(request.getParameter("pass"));
+		memberDetailDto.setEmailid(request.getParameter("emailid"));
+		memberDetailDto.setEmaildomain(request.getParameter("emaildomain"));
+		memberDetailDto.setTel1(request.getParameter("tel1"));
+		memberDetailDto.setTel2(request.getParameter("tel2"));
+		memberDetailDto.setTel3(request.getParameter("tel3"));
+		memberDetailDto.setZipcode(request.getParameter("zipcode"));
+		memberDetailDto.setAddress(request.getParameter("address"));
+		memberDetailDto.setAddressDetail(request.getParameter("address_detail"));
 		
-		String act = request.getParameter("act");
-
-		if("mvjoin".equals(act)) {
-			response.sendRedirect("/membermvc/user/member/member.jsp");
-		} else if("mvlogin".equals(act)) {
-			response.sendRedirect("/membermvc/user/login/login.jsp");
-		} else if("idcheck".equals(act)) {
-			String sid = request.getParameter("sid");
-			String resultXML = MemberServiceImpl.getMemberService().idCheck(sid);
-			
-			response.setContentType("text/xml;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.print(resultXML);
-			
-		} else if("zipsearch".equals(act)) {
-			String doro = request.getParameter("doro");
-//			System.out.println("검색 도로명 : " + doro);
-			String resultXML = MemberServiceImpl.getMemberService().zipSearch(doro);
-//			System.out.println(resultXML);
-			response.setContentType("text/xml;charset=UTF-8");
-			PrintWriter out = response.getWriter();
-			out.print(resultXML);
-		} else if("register".equals(act)) {
-			MemberDetailDto memberDetailDto = new MemberDetailDto();
-			memberDetailDto.setName(request.getParameter("name"));
-			memberDetailDto.setId(request.getParameter("id"));
-			memberDetailDto.setPass(request.getParameter("pass"));
-			memberDetailDto.setEmailid(request.getParameter("emailid"));
-			memberDetailDto.setEmaildomain(request.getParameter("emaildomain"));
-			memberDetailDto.setTel1(request.getParameter("tel1"));
-			memberDetailDto.setTel2(request.getParameter("tel2"));
-			memberDetailDto.setTel3(request.getParameter("tel3"));
-			memberDetailDto.setZipcode(request.getParameter("zipcode"));
-			memberDetailDto.setAddress(request.getParameter("address"));
-			memberDetailDto.setAddressDetail(request.getParameter("address_detail"));
-			
-			int cnt = MemberServiceImpl.getMemberService().registerMember(memberDetailDto);
-			System.out.println("cnt === " + cnt);
-		} else if("".equals(act)) {
-			
-		} else if("".equals(act)) {
-			
-		} else if("".equals(act)) {
-			
-		} else if("".equals(act)) {
-			
-		} else if("".equals(act)) {
-			
+		int cnt = MemberServiceImpl.getMemberService().registerMember(memberDetailDto);
+		if(cnt != 0) {
+			request.setAttribute("userInfo", memberDetailDto);
+			path = "/user/member/registerok.jsp";
+		} else {
+			path = "/user/member/registerfail.jsp";
 		}
+		return path;
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding(SiteConstance.ENCODE);
-		doGet(request, response);
+	public String login(HttpServletRequest request, HttpServletResponse response) {
+		String path = "/index.jsp";
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		
+		MemberDto memberDto = MemberServiceImpl.getMemberService().loginMember(id, pass);
+		if(memberDto != null) {
+			///////////////////// Cookie ///////////////////
+			String idsv = request.getParameter("idsave");
+			if("idsave".equals(idsv)) {
+				Cookie cookie = new Cookie("kid_inf", id);
+				cookie.setDomain("localhost");
+				cookie.setPath(request.getContextPath());
+				cookie.setMaxAge(60*60*24*365*50);
+				response.addCookie(cookie);
+			} else {
+				Cookie cookie[] = request.getCookies();
+				if(cookie != null) {
+					for(Cookie c : cookie) {
+						if("kid_inf".equals(c.getName())) {
+							c.setDomain("localhost");
+							c.setPath(request.getContextPath());
+							c.setMaxAge(0);
+							response.addCookie(c);
+							break;
+						}
+					}
+				}
+			}
+			
+			////////////////////////////////////////////////
+			///////////////////// sesstion ///////////////////
+			HttpSession session = request.getSession();
+			session.setAttribute("userInfo", memberDto);
+			///////////////////////////////////////////////////
+			path = "/user/login/loginok.jsp";
+		} else {
+			path = "/user/login/loginfail.jsp";
+		}
+		return path;
 	}
 
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+//		session.setAttribute("userInfo", null);
+//		session.removeAttribute("userInfo");
+		session.invalidate();
+		return "/user/login/login.jsp";
+	}
+	
 }
+
+
+
+
+
+
+
